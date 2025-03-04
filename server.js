fbbc5b57ca1d3dc9ad7ef5179e86718c29c853ec
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-03-04 17:55:57 +0200

    server

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: 'https://fin-sync.onrender.com'  
+}));
 
 // Basic route to check server
 app.get('/', (req, res) => {
