e18aed76912365a17fada6631083815b54b4d3d9
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-03-06 09:29:39 +0200

    authroutr

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { createSuperAdmin, signInUser, createAdmin, signupAdmin, signOutuser, getAllAdmins, deleteAdmin} = require('../controllers/authController');
-const { requireSuperAdmin } = require('../utils/auth');
+const { requireSuperAdmin, requireAuth } = require('../utils/auth');
 
 router.post('/create-super-admin', createSuperAdmin);
 router.post('/signin', signInUser);
 router.post('/create-admin', requireSuperAdmin, createAdmin); 
 router.post('/signup-admin', signupAdmin); // Admins can sign up without approval
 router.post('/signout' , signOutuser);
-router.get('/admins', requireSuperAdmin, getAllAdmins);
-router.delete('/admin/:adminId', requireSuperAdmin, deleteAdmin);
+router.get('/admins', requireAuth, requireSuperAdmin, getAllAdmins);
+router.delete('/admin/:adminId', requireAuth, requireSuperAdmin, deleteAdmin);
 
 module.exports = router;
